4451f6b6cf5dea8fa2059804a2a4f6d9d3de7f19
Aster <aster@vers.site>
2017-01-26 04:07:55 +0000

    🐣 Add sample images for rpg maker xp

--- projects/tilemap-atlas/src/utils/mod.rs
+++ projects/tilemap-atlas/src/utils/mod.rs
@@ -1,16 +1,25 @@
-use image::ImageResult;
+use image::{GenericImageView, ImageResult};
 use std::path::Path;
 
-pub fn decompose_grid_sequence_frame<P>(
-    _path: P,
-    _start: (usize, usize),
-    _end: (usize, usize),
-    _is_horizontal: bool,
-) -> ImageResult<()>
+pub fn decompose_image_grid_by_cells<P>(path: P, cols: u32, rows: u32) -> ImageResult<()>
 where
     P: AsRef<Path>,
 {
-    todo!()
+    let path = path.as_ref().canonicalize()?;
+    let dir = path.parent().expect("The path must have a parent directory");
+    let name = path.file_stem().expect("The path must have a file name");
+    let image = image::open(&path)?;
+    let (width, height) = image.dimensions();
+    let cell_width = width / cols;
+    let cell_height = height / rows;
+    for row in 0..rows {
+        for col in 0..cols {
+            let view = image.view(col * cell_width, row * cell_height, cell_width, cell_height);
+            let mut out = dir.join(format!("{}-{}-{}.png", name.to_str().unwrap(), col, row));
+            view.save(&out)?;
+        }
+    }
+    Ok(())
 }
 
 pub struct AnimationSlice {}
